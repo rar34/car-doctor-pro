9ac8106b8d283e3ca3846ef0dce44110591c027c
rar34 <ra.cse34@gmail.com>
2024-09-20 23:13:53 +0600

    created a primary color with daisyui

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h2 className="text-3xl font-bold text-purple-800">Car Doctor pro</h2>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      car doctor pro
+      <button className="btn btn-primary">Submit</button>
+       
     </div>
   );
 }
