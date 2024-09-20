9c3f16e9f99dc9d9551079488ebc50a75a08d28a
rar34 <ra.cse34@gmail.com>
2024-09-21 00:13:27 +0600

    change btn primary color and hover color

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,10 +21,14 @@ module.exports = {
       {
         carDoctorTheme: {
           "primary": "#FF3811",
-          "secondary": "#f6d860",
-          "accent": "#37cdbe",
-          "neutral": "#3d4451",
-          "base-100": "#ffffff",
+          "secondary": "teal",
+
+          ".btn-primary": {
+            "color": "#fff"
+          },
+          ".btn-outline.btn-primary:hover" : {
+            "color": "#fff"
+          }
         },
       },
       "dark",
