1f040bbce40cbb902e4bdc27581df39163df1982
rar34 <ra.cse34@gmail.com>
2024-09-21 11:59:34 +0600

    added banner

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Homepage = () => {
     return (
-        <div className='h-screen'>
-            Home page
+        <div className='h-screen container mx-auto my-10'>
+            <Banner />
         </div>
     );
 };
+
 
 export default Homepage;
